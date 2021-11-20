38467f7498b14b25cce3928e2965a796883c82c3
Hawkang <378284409@qq.com>
2021-11-20 20:44:34 +0000

    create a new file readme.md, and changed the pytest2.py nothing special hahaha

--- pytest2.py
+++ pytest2.py
@@ -1,11 +1,2 @@
-import torch
-first_name = 'ADA'
-last_name = 'LOVELACE'
-full_name = f"{first_name} {last_name}"
-print(f"Hello,{full_name.title()}")
-
-a = torch.arange(6).reshape((1, 2, 3))
-b = torch.arange(2).reshape((2, 1))
-print(a)
-print(b)
-b * a
+canyouseeme
+canyouseeme
